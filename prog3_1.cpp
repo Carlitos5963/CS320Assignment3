49422f645d09221b8d1fac7c64711770fca66e20
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-27 17:20:36 -0700

    First commit

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -19,11 +19,11 @@ lua_State *L;
 int main(int argc, char *argv[])
 {
 
+	cout << "Assignment #3-1, Jose Carlos Gomez, JoseCarlosGomez69@gmail.com" << endl;
 	string fileName;
 	
 	if(argc > 1){
 		fileName = argv[1];
-		cout << fileName << endl;
 	}
 
 
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
 	L = luaL_newstate();
 	luaL_openlibs(L);
-	//luaL_dofile(L, fileName);
+	luaL_dofile(L, fileName);
 
 	lua_close(L);
 
