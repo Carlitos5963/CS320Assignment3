80533081c9093b375488bcfee4400db8c7d192b2
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-27 18:19:23 -0700

    First commit

--- prog3_1.cpp
+++ prog3_1.cpp
@@ -29,9 +29,9 @@ int main(int argc, char *argv[])
 
 	L = luaL_newstate();
 	luaL_openlibs(L);
-	luaL_dofile(L, "dofile(fileName)");
+	luaL_dofile(L, "test.lua");
 
-	lua_pcall(L, 0, LUA_MULTRET, 0);
+	//lua_pcall(L, 0, LUA_MULTRET, 0);
 
 	lua_close(L);
 
