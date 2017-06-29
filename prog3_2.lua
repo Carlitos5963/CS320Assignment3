a07cdfef95b38bfa4fded8629ffaedc50c072e6d
Jose Carlos Gomez <josecarlosgomez69@gmail.com>
2017-06-29 02:03:21 -0700

    Another commit

--- prog3_2.lua
+++ prog3_2.lua
@@ -23,4 +23,27 @@ local function InfixToPostfix( str )
 return postfix
 end
 
-InfixToPostfix(str)
+local function InfixToPostfix( str )
+	stack = {}
+	postfix = ""
+	for i in string.gmatch(str, "%S+") do
+		if (tonumber(i) ~= nil) then
+			postfix = postfix .. i
+		else
+			if(i == "*" || i == "/") then
+				if(stack[#stack] == "+" || stack[#stack] == "-") then
+					table.insert(stack, i)
+				else
+					postfix = postfix .. i
+					table.remove(stack, i)
+				end
+			else
+				if(stack[#stack] == "+" || stack[#stack] == "-") then
+					postfix = postfix .. i
+					table.remove(stack, i)
+				end
+			end
+		end
+	end
+return postfix
+end
